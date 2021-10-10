c38522e2f9ad27a19827e1602b156510b80b06d0
kooku19 <kooku190642@gmail.com>
2021-10-10 16:14:01 +0700

    updeatHomeAdmin

--- src/pages/HomeAdmin.vue
+++ src/pages/HomeAdmin.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="q-pa-md ">
-    <div class="text-white " style="border-radius : 10px ;background-color: #1794A5 ;">
-      <q-toolbar >
-        <q-toolbar-title></q-toolbar-title>
-
-        <q-btn flat round dense icon="sim_card" class="q-mr-xs" />
-        <q-btn flat round dense icon="gamepad" />
+  <div class="q-pa-md">
+    <div
+      class="text-white"
+      style="border-radius : 10px ;background-color: #1794A5 ;50px"
+    >
+      <q-toolbar>
+        <q-toolbar-title container style="height: 60px"
+          ><q-img
+            src="../assets/Alumnet.png"
+            width="60px"
+            style="margin-left"
+          /> </q-toolbar-title
+        ><q-input
+          v-model="search"
+          filled
+          placeholder="Search"
+          style="height: 55px : margin-left"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        
       </q-toolbar>
     </div>
   </div>
+  <div>
+    <q-card class="q-ma-sm" >
+          <!-- <img src="https://cdn.quasar.dev/img/mountains.jpg"> -->
+
+          <q-card-section>
+            <div class="text-h6">Card #</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+        </q-card>
+    </div>
+
 </template>
-<script>
-
-</script>
-<style>
-
-  .bg-image {
-    background-image: url("../assets/psu.jpg");
-    background-repeat: no-repeat;
-    background-size:cover;
-    background-position: center;
-  }
-
-
+<style lang="sass" scoped>
+.example-item
+  height: 500px
+  width: 500px
 </style>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    return {
+      search: ref(""),
+    };
+  },
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -47,6 +47,7 @@ const routes = [
         component: () => import('pages/HomeAdmin.vue') 
 
       },
+
     ]
   },
   {
