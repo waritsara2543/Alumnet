471772383636e8af5f25c3ef3a65da54df31f234
Waritsara Wichiansrang <warissara.0039@gmail.com>
2021-10-11 17:39:56 +0700

    update profilepage,notipage

--- src/pages/notificationpage.vue
+++ src/pages/notificationpage.vue
@@ -1,9 +1,40 @@
 <template>
-
   <q-page>
-    
-    <p>Notifications page</p>
-    
+    <div class="q-pa-md">
+      <div class="q-gutter-y-md column" style="max-width: 100%">
+        <q-toolbar
+          class="text-white rounded-borders"
+          style="background:#032030 "
+        >
+         
+          <p
+            style="padding: 15px 0px 0px 20px; font-size: 15px"
+            id="user_name"
+          >
+            Notifications
+          </p>
+
+          
+        </q-toolbar>
+
+
+
+        
+
+        
+        <q-card
+          class="my-card text-white "
+          style="
+            background: linear-gradient(#032030 0%, #1794a5 100%);
+            height: 60px; width: 345px; margin: 0 auto; margin-top:20px;
+          "
+        >
+          <q-card-section class="text-left">
+            <div class="text-subtitle2" id="title_content">title :มอบเงินทุนการศึกษาให้กับผู้สนใจศึกษาต่อในระดับ ปริญญาโท</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
  <script>
@@ -13,7 +44,8 @@ export default {
 
   setup() {
     return {
-     
+      search: ref(""),
+      
     };
   },
 };
--- src/pages/profilepage.vue
+++ src/pages/profilepage.vue
@@ -12,26 +12,70 @@
           <q-card-section class="text-center" >
            
              
-                <q-avatar class="q-mr-xs" id="image_profile" style="position:relative; width:100px; height:100px; top:-60px; background:white;">
+                <q-avatar class="q-mr-xs" id="image_profile" style="position:relative; width:100px; height:100px; top:-60px; left:10px; background:white;">
                   <img src="../assets/man.png" style="border-style: solid;"  />
                 </q-avatar>
+                <q-icon @click="toavatar" name="add_circle" style="position:relative;bottom:20px; right:20px ;font-size:30px" />
               
                 <div class="text-h6" id="user_name" style="margin-top: -40px">Waritsara Wichiansrang</div>
                 <div class="text-h6"  style="border-top: 3px solid #ffffff; padding:20px ;margin-top: 20px"></div>
-                <div class="" style="font-size: 10px" id="date">18 May</div>
-                <div style="text-align: center">
+            
+          </q-card-section>
+          <q-card-section class="text-left" >
+            <div class="row">
+              <div class="col-1">
+                  <q-icon name="school" />
+              </div>
+              <div class="col" id="graduate">
+               Graduation Software Engineering 2016 at PSU Phuket Campus <q-icon  @click="prompt = true" name="edit" />
+              </div>
+               
+            </div>
+
+            <div class="row">
+              <div class="col-1">
                   <q-icon name="business_center" />
-                </div>
-                <div class="" style="text-align: center" id="update_working">
-                  Software Egineer at Agoda
-                </div>
-                <div
-                  class=""
-                  style="font-size: 12px; text-align: center"
-                  id="update_date"
-                >
-                  18 May 2020
-                </div>
+              </div>
+              <div class="col" id="workplace">
+                Software Engineer at Focal Solution<q-icon name="edit" />
+              </div>
+               
+            </div>
+
+            <div class="row">
+              <div class="col-1">
+                  <q-icon name="location_on" />
+              </div>
+              <div class="col" id="province">
+                Lives in Bangkok, Thailand<q-icon name="edit" />
+              </div>
+               
+            </div>
+
+            <div class="row">
+              <div class="col-1">
+                  <q-icon name="favorite" />
+              </div>
+              <div class="col" id="status">
+                single<q-icon name="edit" />
+              </div>
+               
+            </div>
+
+            <div class="row">
+              <div class="col-1">
+                  <q-icon name="description" />
+              </div>
+              <div class="col" id="epigram">
+                มีดบาด<q-icon name="edit" />
+              </div>
+               
+            </div>
+                
+
+             <div class="text-center">
+              <q-icon style="font-size: 40px" name="add_circle" />
+            </div>
               
             
           </q-card-section>
@@ -42,7 +86,10 @@
           <div class="col" style="padding:10px 5px 5px 0px">
             <q-card class="my-card text-white text-center" style="
             background: linear-gradient(#032030 0%, #1794a5 100%);height: 100px;margin: 0 auto; ">
+            
+              <div class="text-h4" id="" style="padding-top:20px">69</div>
             <div class="text-caption" id="" style="">Following</div>
+            
 
             </q-card>
 
@@ -50,6 +97,7 @@
           <div class="col" style="padding:10px 0px 5px 5px">
             <q-card class="my-card text-white text-center" style="
             background: linear-gradient(#032030 0%, #1794a5 100%);height: 100px;margin: 0 auto; ">
+            <div class="text-h4" id="" style="padding-top:20px">54</div>
             <div class="text-caption" id="" style="">Follower</div>
 
             </q-card>
@@ -61,6 +109,7 @@
           <div class="col" style="padding:5px 5px 10px 0px">
             <q-card class="my-card text-white text-center" style="
             background: linear-gradient(#032030 0%, #1794a5 100%);height: 100px;margin: 0 auto; ">
+            <q-icon style="font-size: 40px ;margin-top:20px" name="lock" />
             <div class="text-caption" id="" style="">Account</div>
 
             </q-card>
@@ -69,6 +118,7 @@
           <div class="col" style="padding:5px 0px 10px 5px">
             <q-card class="my-card text-white text-center" style="
             background: linear-gradient(#032030 0%, #1794a5 100%);height: 100px;margin: 0 auto; ">
+            <q-icon style="font-size: 40px ;margin-top:20px" name="attach_file" />
             <div class="text-caption" id="" style="">Contact channel</div>
 
             </q-card>
@@ -87,17 +137,42 @@
         "
       />
     
+    <!-- <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Edit</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input dense v-model="address" autofocus @keyup.enter="prompt = false"  />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add address" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog> -->
     
   </q-page>
 </template>
  <script>
 import { ref } from "vue";
 export default {
-  methods: {},
+  methods: {
+    // backconfirmEmail() {
+    //   this.$router.push({ name: "confirmEmail" });
+    // },
+    toavatar() {
+      this.$router.push({ name: "toavatar" });
+    },
+  },
 
   setup() {
     return {
-     
+     prompt: ref(false),
+
+      address: ref('')
     };
   },
 };
