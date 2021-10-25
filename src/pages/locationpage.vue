53c1fd99015daecd7dbd7ded81d757914df41a64
Waritsara Wichiansrang <warissara.0039@gmail.com>
2021-10-26 01:01:01 +0700

    test map

--- src/pages/locationpage.vue
+++ src/pages/locationpage.vue
@@ -1,9 +1,12 @@
 <template>
-<q-page>
- 
+  <q-page class="flex-center">
+    <TomTomMap city='Thailand, OR' apikey='scQnhiQ71GvRA0HmCrirnmDvngEqCaqw'/>
   </q-page>
 </template>
 
 <script>
- 
+import TomTomMap from 'components/TomTomMap.vue'
+export default {
+  components: { TomTomMap }
+}
 </script>
