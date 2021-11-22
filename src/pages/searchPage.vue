29c92935e0e8f17180109c0889700befa3eb313c
Waritsara Wichiansrang <warissara.0039@gmail.com>
2021-11-22 18:11:07 +0700

    -

--- src/pages/searchPage.vue
+++ src/pages/searchPage.vue
@@ -1,11 +1,22 @@
 <template>
   <q-page>
     <inputsearchComponent></inputsearchComponent>
+    <q-btn
+            @click="result"
+            rounded
+            label="SEARCH"
+            class="full-width text-white"
+            style="
+              font-size: 15px;
+              background: linear-gradient(#b42425 0%, #b42425 100%);
+            "
+          />
     <resultsearchComponent @click="result"></resultsearchComponent>
   </q-page>
 </template>
  <script>
 import { ref } from "vue";
+import { getSearch } from '../api/api'
 import inputsearchComponent from "../components/inputsearch.vue";
 import resultsearchComponent from "../components/resultsearch.vue";
 export default {
@@ -14,13 +25,22 @@ export default {
     resultsearchComponent,
   },
   methods: {
-    result(){
-      this.$router.push({ name: "resultSearch" });
+        //     async searchs(){
+        //    let testcase =  await getSearch(this.search,this.search)
+        //    console.log(testcase);
+        //     console.log(this.search);
+        // }
+    // },
+    async result(){
+      let testcase =  await getSearch(this.search,this.search)
+      console.log(testcase);
+      console.log(this.search);
+      // this.$router.push({ name: "resultSearch" });
     }
     ,
   },
 
-  setup() {
+  data() {
     return {
       search: ref(""),
     };
